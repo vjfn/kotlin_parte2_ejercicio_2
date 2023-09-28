0be7db299d35351d114bf631827f5907869e3982
VICTOR <yo@casa.com>
2023-09-28 20:08:53 +0200

    Parte 2 Ejercicio2 terminado desde casa

--- app/src/main/java/com/example/kotlin_parte2_ejercicio_2/Cuenta.kt
+++ app/src/main/java/com/example/kotlin_parte2_ejercicio_2/Cuenta.kt
@@ -1,11 +1,40 @@
 package com.example.kotlin_parte2_ejercicio_2
 
-class Cuenta (val numeroCuenta:String,val saldo:Double, val propietario:Persona){
+//class Cuenta (val numeroCuenta:String,val saldo:Double, val propietario:Persona){
+//
+//    constructor():this("",0.0,Persona("","",""))
+//
+//
+//    override fun toString(): String {
+//        return "Cuenta(numeroCuenta='$numeroCuenta', Saldo='$saldo', Propietario=$propietario)"
+//    }
+//}
+
+class Cuenta(val numeroCuenta: String, var saldo: Double, var propietario: Persona) {
 
     constructor():this("",0.0,Persona("","",""))
+    init {
+        require(saldo >= 0) { "El saldo no puede ser menor que 0" }
+    }
 
+    fun transaccion(cantidad: Double, tipoTransaccion: String) {
+        when (tipoTransaccion) {
+            "reintegro" -> {
+                require(cantidad >= 0) { "La cantidad a reintegrar no puede ser negativa" }
+                saldo -= cantidad
+                println("Reintegro: $cantidad")
+            }
+            "ingreso" -> {
+                require(cantidad >= 0) { "La cantidad a ingresar no puede ser negativa" }
+                saldo += cantidad
+                println("Ingreso: $cantidad")
+            }
+            else -> println("Tipo de transacción no válido")
+        }
+        println("Nuevo saldo: $saldo")
+    }
 
     override fun toString(): String {
-        return "Cuenta(numeroCuenta='$numeroCuenta', Saldo='$saldo', Propietario=$propietario)"
+        return "Número de Cuenta: $numeroCuenta, Saldo: $saldo, Propietario: ${propietario.nombre} ${propietario.apellido}"
     }
 }
--- app/src/main/java/com/example/kotlin_parte2_ejercicio_2/Main.kt
+++ app/src/main/java/com/example/kotlin_parte2_ejercicio_2/Main.kt
@@ -1,13 +1,32 @@
 package com.example.kotlin_parte2_ejercicio_2
 
-fun main(){
+//fun main(){
+//
+//    val Persona1 = Persona("Pepe", "Lopez", 123456789 )
+//
+//    val Cuenta1 = Cuenta("N786",740.20, Persona1)
+//
+//    println(Persona1)
+//    println(Cuenta1)
+//
+//
+//}
 
-    val Persona1 = Persona("Pepe", "Lopez", 123456789 )
+fun main() {
+    val persona1 = Persona("Juan", "Perez", "123456789")
+    val persona2 = Persona("Maria", "Gomez", "987654321")
 
-    val Cuenta1 = Cuenta("N786",740.20, Persona1)
+    val cuenta1 = Cuenta("12345", 1000.0, persona1)
+    val cuenta2 = Cuenta("67890", 500.0, persona2)
 
-    println(Persona1)
-    println(Cuenta1)
+    cuenta1.transaccion(200.0, "reintegro")
+    cuenta1.transaccion(300.0, "ingreso")
 
+    cuenta2.transaccion(100.50, "reintegro")
+    cuenta2.transaccion(300.87, "ingreso")
 
+    println("Datos de Persona 1: ${persona1.toString()}")
+    println("Datos de Persona 2: ${persona2.toString()}")
+    println("Datos de Cuenta 1: ${cuenta1.toString()}")
+    println("Datos de Cuenta 2: ${cuenta2.toString()}")
 }
